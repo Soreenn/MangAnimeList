c2d626bd2eb2b723a2f26ebae5617581ebb91193
Luke <61788453+LukeCornaz@users.noreply.github.com>
2021-11-26 13:51:25 +0100

    Adding anime classes

---
+++ MangAnimeList/AnimeSeason.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangAnimeList
+{
+    public class AnimeSeason
+    {
+        public string season { get; set; }
+        public int? year { get; set; }
+    }
+}
---
+++ MangAnimeList/Datum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangAnimeList
+{
+    public class Datum
+    {
+        public string[] sources { get; set; }
+        public string title { get; set; }
+        public string type { get; set; }
+        public int episodes { get; set; }
+        public string status { get; set; }
+        public AnimeSeason animeSeason { get; set; }
+        public string picture { get; set; }
+        public string thumbnail { get; set; }
+        public string[] synonyms { get; set; }
+        public string[] relations { get; set; }
+        public string[] tags { get; set; }
+    }
+}
