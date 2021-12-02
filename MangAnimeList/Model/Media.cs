43204f2472850c2abad67aeea39e27bd7effa6b6
Axwells <61867476+Axwells@users.noreply.github.com>
2021-12-02 09:18:28 +0100

    Update the content of Media.cs

--- MangAnimeList/Model/Media.cs
+++ MangAnimeList/Model/Media.cs
@@ -8,5 +8,44 @@ namespace MangAnimeList.Model
 {
     class Media
     {
+        private string _title;
+        private string _status;
+        private int _startYear;
+        private List<string> _tags;
+        private string _cover;
+
+        public Media(string title, string status, int startYear, string cover)
+        {
+            _title = title;
+            _status = status;
+            _startYear = startYear;
+            _tags = new List<string>();
+            _cover = cover;
+        }
+
+        public string Title
+        {
+            get { return _title; }
+        }
+
+        public string Status
+        {
+            get { return _status; }
+        }
+
+        public int StartYear
+        {
+            get { return _startYear; }
+        }
+
+        public List<string> Tags
+        {
+            get { return _tags; }
+        }
+
+        public string Cover
+        {
+            get { return _cover; }
+        }
     }
 }
