11209464e6357d7cc7deec37981ac78a8931fd1e
Søren <35235150+Soreenn@users.noreply.github.com>
2021-12-03 14:45:17 +0100

    Trying to fix repo

---
+++ MangAnimeList/MangAnimeList/Model/Media.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangAnimeList
+{
+    public class Media
+    {
+        private List<string> _title;
+        private string _status;
+        private int _releaseYear;
+        private List<string> _tags;
+        private string _cover;
+
+        public Media(List<string> title ,string status, int releaseYear, List<string> tags, string cover)
+        {
+            _title = title.ToList();
+            _tags = tags.ToList();
+            _status = status;
+            _releaseYear = releaseYear;
+            _cover = cover;
+        }
+
+        public List<string> Title
+        {
+            get { return _title; }
+        }
+
+        public string Status
+        {
+            get { return _status; }
+        }
+
+        public int StartYear
+        {
+            get { return _releaseYear; }
+        }
+
+        public List<string> Tags
+        {
+            get { return _tags; }
+        }
+
+        public string Cover
+        {
+            get { return _cover; }
+        }
+    }
+}
---
+++ MangAnimeList/Scripts/GetMangaScript/GetMangaScript.py
@@ -0,0 +1,50 @@
+import requests
+import json
+import time
+id = 30001
+# Here we define our query as a multi-line string
+query = '''
+query ($id: Int) { # Define which variables will be used in the query (id)
+  Media (id: $id, type: MANGA) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
+        id
+    title{
+      english
+      romaji
+      native
+    }
+    averageScore
+    volumes
+    chapters
+    status
+    startDate{
+      year
+      month
+      day
+    }
+    tags{
+      name
+    }
+    coverImage{
+      extraLarge
+    }
+    bannerImage
+  }
+}
+'''
+# Define our query variables and values that will be used in the query request
+variables = {
+    'id': 30001
+}
+url = 'https://graphql.anilist.co'
+# Make the HTTP Api request
+while id < 40000:
+  variables['id'] = id
+  response = requests.post(url, json={'query': query, 'variables': variables}).json
+  datas = response()
+  if not "errors" in datas:
+    print(id)
+    with open('list.json', 'a') as f:
+      json.dump(response(), f)
+    time.sleep(0.5)
+  id += 1
+  
