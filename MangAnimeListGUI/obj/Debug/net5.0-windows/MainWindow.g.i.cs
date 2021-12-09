e313abd07c74c47fe1a14053e9dff67f51fa0a74
Axwells <61867476+Axwells@users.noreply.github.com>
2021-12-09 08:15:43 +0100

    Update the List WPF window

--- MangAnimeListGUI/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ MangAnimeListGUI/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -117,7 +117,7 @@ namespace MangAnimeListGUI {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MangAnimeListGUI;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MangAnimeListGUI;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
