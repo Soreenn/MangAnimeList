2d600c44044904c5c8f5fb2d4e1b2fa7becfd77d
Axwells <61867476+Axwells@users.noreply.github.com>
2022-01-13 09:22:15 +0100

    Add the Script for the DataBase

---
+++ Scripts/DBScript/MangAnimeListBDScript.sql
@@ -0,0 +1,70 @@
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           8.0.27 - MySQL Community Server - GPL
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Listage de la structure de la base pour manganimelist
+DROP DATABASE IF EXISTS `manganimelist`;
+CREATE DATABASE IF NOT EXISTS `manganimelist` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `manganimelist`;
+
+-- Listage de la structure de la table manganimelist. animes
+DROP TABLE IF EXISTS `animes`;
+CREATE TABLE IF NOT EXISTS `animes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table manganimelist.animes : ~0 rows (environ)
+DELETE FROM `animes`;
+/*!40000 ALTER TABLE `animes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `animes` ENABLE KEYS */;
+
+-- Listage de la structure de la table manganimelist. mangas
+DROP TABLE IF EXISTS `mangas`;
+CREATE TABLE IF NOT EXISTS `mangas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `user_id` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table manganimelist.mangas : ~0 rows (environ)
+DELETE FROM `mangas`;
+/*!40000 ALTER TABLE `mangas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mangas` ENABLE KEYS */;
+
+-- Listage de la structure de la table manganimelist. users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `userType` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table manganimelist.users : ~2 rows (environ)
+DELETE FROM `users`;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `username`, `password`, `userType`) VALUES
+	(1, 'test', '123', 1),
+	(2, 'coucou', 'kiki', 1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
